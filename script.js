e3f73c1de1f984580bea6c096c242a4f47478405
asif-96 <shuyeb.dk@gmail.com>
2021-09-02 00:01:38 +0600

    working on index file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let total_price, sub_total, price1, price2;
 let increment1, increment, decrement1, decrement, crnt_price1, crnt_price2,
-    num, num1, converter, converter1;
+    num, num1, converter, converter1, subTotal;
 
 const plusBtn1 = document.querySelector('#plus-btn1');
 const minusBtn1 = document.querySelector('#minus-btn1');
@@ -23,17 +23,25 @@ crnt_price2 = document.getElementById('crnt-price2').innerHTML;
 // console.log(crnt_price2);
 let p = parseInt(crnt_price2);
 // console.log(p);
+subTotal = document.getElementById('subTotal').innerHTML;
+sub_total = parseInt(subTotal);
+console.log(sub_total)
 
 
 // Positive Calculation
 function positiveCalc1() {
     num = document.querySelector('.input1').value;
+    // const caseInput = document.querySelector('.input1');
+    // const caseCount = parseInt(caseInput.value);
+    // const caseNewCount = caseCount + 1;
+    // caseInput.value = caseNewCount;
     // let num2 = document.querySelector('.input2').value;
     // console.log(num1);
     increment = 1 + parseInt(num);
     converter = increment.toString();
     document.querySelector('.input1').value = converter;
     crnt_price1 = v * increment;
+    sub_total = sub_total + crnt_price1;
     // console.log(crnt_price1)
     document.getElementById('crnt-price1').innerHTML = crnt_price1;
 }
@@ -45,6 +53,7 @@ function positiveCalc2() {
     converter1 = increment1.toString();
     document.querySelector('.input2').value = converter1;
     crnt_price2 = p * increment1;
+    sub_total = sub_total + crnt_price2;
     document.getElementById('crnt-price2').innerHTML = crnt_price2;
 }
 
@@ -62,7 +71,8 @@ function negativeCalc1() {
         document.querySelector('.input1').value = converter;
         // let updataValue = crnt_price1 - v;
         crnt_price1 = v * decrement;
-        // console.log(crnt_price1)
+        sub_total = sub_total - crnt_price1;
+    // console.log(crnt_price1)
         document.getElementById('crnt-price1').innerHTML = crnt_price1;
     }
 }
@@ -78,6 +88,7 @@ function negativeCalc2() {
         converter1 = decrement1.toString();
         document.querySelector('.input2').value = converter1;
         crnt_price2 = p * decrement1;
+        sub_total = sub_total - crnt_price2;
         document.getElementById('crnt-price2').innerHTML = crnt_price2;
     }
 }
@@ -86,9 +97,10 @@ function negativeCalc2() {
     
 // }
 
-sub_total = price1 + price2;
-let subTotal = document.getElementById('subTotal').innerHTML;
-subTotal = parseInt(subTotal) + sub_total;
-document.getElementById('subTotal').innerHTML = subTotal;
+// sub_total = parseInt(crnt_price1) + parseInt(crnt_price2);
+console.log(sub_total)
+// let subTotal = document.getElementById('subTotal').innerHTML;
+// subTotal = parseInt(subTotal) + sub_total;
+document.getElementById('subTotal').innerHTML = sub_total;
 
 // console.log(increment);
